faa5cf04c1a4cd0ee6c1166a9ab293abfd7ecda9
Glitch (xrproject) <none>
2021-07-07 14:29:55 +0000

    😄💓 Updated with Glitch - make character with gltf and synchronize moving with camera

--- server.js
+++ server.js
@@ -8,6 +8,88 @@
 const express = require("express");
 const socket = require("socket.io");
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection',() => {});
+
+
+const startX = 0;
+const startY = 0;
+const startZ = 0;
+
+class User{
+  constructor(socket){
+    this.socket = socket;
+    this.x = startX;
+    this.y = startY;
+    this.z = startZ;
+  }
+  
+  get id() {
+    return this.socket.id;
+  }
+}
+
+let users = [];
+let userMap = {};
+
+function join(socket){
+  let user = new User(socket);
+  
+  users.push(user)
+  userMap[socket.id] = user;
+  
+  return user;
+}
+
+function leave(socket){
+  for(let i = 0; i< users.length; i++){
+    if(users[i].id = socket.id){
+      users.splice(i,1);
+      break;
+    }
+  }
+  delete userMap[socket.id];
+}
+
+io.on('connection', function(socket) {
+  console.log(`${socket.id}님이 입장하였습니다.`);
+  
+  socket.on('disconnect', function(reason){
+    console.log(`${socket.id}님이 ${reason}의 이유로 퇴장하였습니다.`);
+    leave(socket)
+    socket.broadcast.emit('leave_user', socket.id);
+  });
+  
+  let newUser = join(socket);
+  socket.emit('user_id', socket.id);
+  
+  for(let i = 0; i < users.length; i++){
+    let user = users[i];
+    socket.emit('join_user', {
+      id: user.id,
+      x: user.x,
+      y: user.y,
+      z: user.z
+    });
+  }
+  
+  socket.broadcast.emit('join_user', {
+    id: socket.id,
+    x: newUser.x,
+    y: newUser.y,
+    z: newUser.z
+  });
+  
+  socket.on('send_location', function(data){
+    socket.broadcast.emit('update_state', {
+      id: data.id,
+      x: data.x,
+      y: data.y,
+      z: data.z
+    });
+  })
+})
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
